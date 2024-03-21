c4c3aaff33383fa04e533f1acaf6fe27d60bdabb
kledisalla <142668349+kledisalla@users.noreply.github.com>
2024-03-21 08:35:49 +0100

    Abstract extraction fix

--- script.py
+++ script.py
@@ -1,45 +1,58 @@
-#!/usr/bin/python3
 import re
 import sys
-import os
 
-def get_abstracts_file(pubmed_file):
-    #open the input file provided in read mode and the ouput that will be created in write mode
-    with open(input_file, "r") as infile:
-        try:
-            with open(output_file, "w") as outfile:
-                entry_found = False #Set a flag to keep track of entries
-                current_entry = '' #initialize a string that stores each entry
-                for line in infile:
-                    #Find each entry this is done by finding the lines that start with number: 
-                    if re.match("^[0-9]+:[\s\w+.]+[\s\d+\s\w+;\d].+", line.strip()):
-                        entry_found = True #if found, this is a new entry so set the flag to True
-
-                    if entry_found:
-                        #If the line Related links is encountered set the flag to false, clear the current entry and write
-                        #the string with the biggest length, which will always be the Abstract
-                        if line.startswith("Related"):
-                            outfile.write(max(current_entry.split("\n\n"), key=len) + "\n")
-                            current_entry = ''
-                            entry_found = False
-                        else: #Else concatinate the lines till "Related links" is encountered
-                            current_entry += line
-        except IOError as err:
-            print(err)
-            
-#Main program
-try:
-    input_file = sys.argv[1]
-    output_file = os.path.splitext(input_file)[0] + "_abstracts.txt"
-    #Call function to create a file of abstracts from the input
-    get_abstracts_file(input_file)
+def extract_abstracts(input_file,output_file):
+    ''' 
+    Function that extracts the header of a medline article and its abstract.
+    inputs:Pubmed file , desired output file
+    '''
     
-#If the user didn't provide an input file raise an Index error
-except IndexError:
-    print("Usage:./main.py <pubmed file>")
-    sys.exit(1)
-#If there is a issue openning the input file raise an IO error
-except IOError as err:
-    print(err)
-    sys.exit(1)
+    entry_start_pattern = r'^\d+: \w+.*'
+    abstract_pattern = r'^PMID: \d* +\[PubMed - indexed for MEDLINE]'
+    
+    # Initialize abstract count
+    entry_count = 0
 
+    # Flag to check if the previous line is empty
+    prev_line_empty = True
+
+    # Paragraph buffer that holds the last paragraph
+    paragraph_buffer=''
+    
+    for line in input_file:
+        
+            if re.match(entry_start_pattern, line):  # Check if line matches the starting line of an entry pattern
+                
+                entry_count += 1  # Increment abstract count
+                
+                # Check if the abstract count matches the number in the line and the previous line was empty
+                if entry_count == int(line.split(':')[0]) and prev_line_empty:
+                    
+                    output_file.write(line.strip() + '\n\n')  # Write the line to output file
+                    
+                    prev_line_empty = False  # Set flag to False since the current line is not empty
+                else:
+                    entry_count -= 1  # Decrement abstract count as this line is not a new entry
+                    
+            elif line.strip() == '':  # Check if the line is empty
+                prev_line_empty = True  # Set flag to True
+                paragraph_buffer += line
+                
+            else:
+                if re.match(abstract_pattern,line):
+                    output_file.write(paragraph_buffer.split('\n\n')[-2] + '\n\n') 
+                    paragraph_buffer = '' 
+                    
+                else:
+                    
+                    prev_line_empty = False  # Set flag to False as this line is not empty
+                    paragraph_buffer += line
+                    
+    #Close pubmed file
+    input_file.close()
+    
+#Main program
+
+infile=open(sys.argv[1])
+outfile=open(sys.argv[2],"w")
+extract_abstracts(infile,outfile)
